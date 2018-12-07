d656caadcd2ad103d517f1f2548530b165e1bb3f
Muppana, Revanth <157719@s020rmc148-22.local>
2018-12-07 13:26:42 -0600

    Initial Commit

---
+++ Connect4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Muppana, Revanth on 12/7/18.
+//  Copyright © 2018 Muppana, Revanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
